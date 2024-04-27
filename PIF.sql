8905c63f535bddac108b4a57750cc53daf656e94
rodvl800 <rodvl800@school.lu>
2024-04-27 22:55:35 +0200

    Login and Register trough new database

--- PIF.sql
+++ PIF.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 26, 2024 at 10:22 PM
+-- Host: localhost
+-- Generation Time: Apr 27, 2024 at 10:54 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Pif`
+-- Database: `PIF`
 --
 
 -- --------------------------------------------------------
@@ -60,12 +60,22 @@ CREATE TABLE `Measurement` (
 
 CREATE TABLE `RecyclingCenter` (
   `CenterCode` varchar(255) NOT NULL,
+  `CenterName` varchar(255) NOT NULL,
   `Address` varchar(255) NOT NULL,
   `PostalCode` int(10) NOT NULL,
   `Town` varchar(255) NOT NULL,
   `Open` time NOT NULL,
   `Close` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `RecyclingCenter`
+--
+
+INSERT INTO `RecyclingCenter` (`CenterCode`, `CenterName`, `Address`, `PostalCode`, `Town`, `Open`, `Close`) VALUES
+('RCESH', 'Esch', '85, Avenue de la Gare', 4130, 'Esch-sur-Alzette', '10:00:00', '15:00:00'),
+('RCGAR', 'Luxembourg-Gare', 'Place de la Gare', 1616, 'Luxembourg', '09:00:00', '19:00:00'),
+('RCLUX', 'Luxembourg-City', '1, rue de Beggen', 112345, 'Luxembourg', '08:30:00', '18:00:00');
 
 -- --------------------------------------------------------
 
@@ -82,18 +92,25 @@ CREATE TABLE `Station` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `Users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
   `Username` varchar(255) NOT NULL,
   `Name` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Type` varchar(255) NOT NULL DEFAULT 'user',
-  `RandomCode` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
+  `IsAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  `RandomCode` varchar(255) DEFAULT NULL,
+  `PasswordHash` varchar(255) NOT NULL,
   `RecCenterCode` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`Username`, `Name`, `IsAdmin`, `RandomCode`, `PasswordHash`, `RecCenterCode`) VALUES
+('2', '2', 0, NULL, '$2y$10$xwFlPCLCZQKJTzGoh4MeV.fXOwaVQ2lFaTH0811iKiQz9YcYjQlrC', 'RCLUX'),
+('3', '3', 0, NULL, '$2y$10$tm.wftCxjuvc84smvLA4GuNAOeElLZH1mVkHjW7F7naha6xg4XQsa', 'RCLUX');
 
 -- --------------------------------------------------------
 
@@ -143,9 +160,9 @@ ALTER TABLE `Station`
   ADD KEY `CenterCode` (`CenterCode`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `Users`
 --
-ALTER TABLE `users`
+ALTER TABLE `Users`
   ADD PRIMARY KEY (`Username`),
   ADD KEY `RecCenterCode` (`RecCenterCode`);
 
@@ -185,7 +202,7 @@ ALTER TABLE `Station`
 -- Constraints for table `Invoice`
 --
 ALTER TABLE `Invoice`
-  ADD CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`InvoiceUsername`) REFERENCES `users` (`Username`),
+  ADD CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`InvoiceUsername`) REFERENCES `Users` (`Username`),
   ADD CONSTRAINT `invoice_ibfk_2` FOREIGN KEY (`InvoiceCenterCode`) REFERENCES `RecyclingCenter` (`CenterCode`);
 
 --
@@ -195,7 +212,7 @@ ALTER TABLE `Measurement`
   ADD CONSTRAINT `measurement_ibfk_1` FOREIGN KEY (`idInvoice`) REFERENCES `Invoice` (`InvoiceID`),
   ADD CONSTRAINT `measurement_ibfk_2` FOREIGN KEY (`StationID`) REFERENCES `Station` (`StationID`),
   ADD CONSTRAINT `measurement_ibfk_3` FOREIGN KEY (`Type`) REFERENCES `Waste` (`Type`),
-  ADD CONSTRAINT `measurement_ibfk_4` FOREIGN KEY (`Username`) REFERENCES `users` (`Username`);
+  ADD CONSTRAINT `measurement_ibfk_4` FOREIGN KEY (`Username`) REFERENCES `Users` (`Username`);
 
 --
 -- Constraints for table `Station`
@@ -204,9 +221,9 @@ ALTER TABLE `Station`
   ADD CONSTRAINT `station_ibfk_1` FOREIGN KEY (`CenterCode`) REFERENCES `RecyclingCenter` (`CenterCode`);
 
 --
--- Constraints for table `users`
+-- Constraints for table `Users`
 --
-ALTER TABLE `users`
+ALTER TABLE `Users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RecCenterCode`) REFERENCES `RecyclingCenter` (`CenterCode`),
   ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`RecCenterCode`) REFERENCES `RecyclingCenter` (`CenterCode`);
 COMMIT;
